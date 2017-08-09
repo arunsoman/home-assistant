11b4ed2ec20723d1742c6a2e4f37d4bf3d356781
Arun Soman <aarunsoman@gmail.com>
2017-08-09 10:42:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,21 @@ sudo adduser --system hosst
 sudo addgroup hosst
 sudo mkdir /srv/homeassistant
 sudo chown hosst:hosst /srv/homeassistant
+
+
+sudo cp hass-daemon /etc/init.d/hass-daemon
+sudo chmod +x /etc/init.d/hass-daemon
+sudo update-rc.d hass-daemon defaults
+
 sudo su -s /bin/bash hosst
 virtualenv -p python3 /srv/homeassistant
 source /srv/homeassistant/bin/activate
 pip3 install --upgrade homeassistant
-sudo cp hass-daemon /etc/init.d/hass-daemon
-sudo chmod +x /etc/init.d/hass-daemon
-sudo update-rc.d hass-daemon defaults
+deactivate
+exit
+
 sudo service hass-daemon install
 sudo service hass-daemon start
 
 #sudo -u hosst -H /srv/homeassistant/bin/hass
+#hass --open-ui
